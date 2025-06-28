5ffd0ffd529dec33938001cdda338463444bbe32
Andris P <andras.petocz@gmail.com>
2025-06-28 13:02:20 +0200

    secret moved to env file

--- phishproof/phishproof/settings.py
+++ phishproof/phishproof/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ae+7x&pvw_m8w&!f2)q^sbwev-b3fv6w(871lt0*ufnmd5zat('
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
