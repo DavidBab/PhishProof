57d1d525ea95920f26cf4621934fb26947d35674
DavidBab <davidbab20@gmail.com>
2025-06-29 17:16:31 +0200

    update svg pictures on the site

--- phishproof/accounts/urls.py
+++ phishproof/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 from .forms import BootstrapLoginForm
-from accounts.views import CustomLoginView
+
 urlpatterns = [
     # path('login/', views.login_view, name='login_view'),  
     # path('register/', views.register_view, name='register_view'),  
